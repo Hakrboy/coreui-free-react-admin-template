953623e5af7562946cd92123d7ade46fecffab77
Debojit Deb <visual-basic@live.com>
2021-01-28 00:26:50 +0530

    date range and select added

--- src/views/salerep/Salerep.js
+++ src/views/salerep/Salerep.js
@@ -1,12 +1,16 @@
-import React, { lazy } from 'react'
+import React, { lazy, Component } from 'react'
 import Select from 'react-select'
+import { RangeDatePicker } from '@y0c/react-datepicker'
+import '@y0c/react-datepicker/assets/styles/calendar.scss'
+import 'moment/locale/en-in'
 import {
-  CBadge,
+  CLabel,
+  CSelect,
   CButton,
   CButtonGroup,
   CCard,
   CCardBody,
-  CCardFooter,
+  CFormGroup,
   CCardHeader,
   CCol,
   CProgress,
@@ -20,9 +24,11 @@ import MainChartExample from '../charts/MainChartExample.js'
 const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
 const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: '1', label: 'ANUSHREE SAREES' },
+  { value: '2', label: 'LUCKY WOMAN' },
+  { value: '3', label: 'HARSHEE CREATION' },
+  { value: '4', label: 'JASLEEN FASHION' },
+  { value: '5', label: 'KASHI KUNJ' }
 ]
 const salerep = () => {
   return (
@@ -33,41 +39,29 @@ const salerep = () => {
             Sale Report
           </CCardHeader>
           <CCardBody>
-            <Select name="selectSm" id="SelectLm" options={options} >
-            </Select>
-
-            {/* <h6>CBreadcrumbRouter wrapper component</h6>
-            <CBreadcrumbRouter routes={routes}/>
-            <h6>Manual</h6>
-            <CBreadcrumb>
-              <CBreadcrumbItem>
-                <CLink>Home</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem active>Library</CBreadcrumbItem>
-            </CBreadcrumb>
-            <CBreadcrumb>
-              <CBreadcrumbItem>
-                <CLink>Home</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem>
-                <CLink>Library</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem active>Data</CBreadcrumbItem>
-            </CBreadcrumb>
-            <CBreadcrumb>
-              <CBreadcrumbItem>
-                <CLink>Home</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem>
-                <CLink>Library</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem>
-                <CLink>Data</CLink>
-              </CBreadcrumbItem>
-              <CBreadcrumbItem active>
-                <span>Bootstrap</span>
-              </CBreadcrumbItem>
-            </CBreadcrumb> */}
+            <CFormGroup>
+            <CRow>
+              <CCol xs="12">
+                <CLabel htmlFor="conum">Selected Company</CLabel>
+                <Select isMulti name="conum" id="conum" options={options} defaultValue={[options[0]]} />
+              </CCol>
+              </CRow>
+              <CRow>
+                <CCol xs="6">
+                <CLabel htmlFor="repchoice">Select Report</CLabel>
+                <CSelect custom name="repchoice" id="repchoice">
+                  <option value="1" selected>SUMMARY BASED SALE REPORT</option>
+                  <option value="2">ITEM BASED SALE REPORT</option>
+                  <option value="3">AGENT BASED SALE REPORT</option>
+                  <option value="4">STATION BASED SALE REPORT</option>
+                </CSelect>
+              </CCol>
+              <CCol xs="6">
+                <CLabel htmlFor="dat">Date Range</CLabel>
+                <RangeDatePicker name="dat" id="dat" locale="en-in"/>
+              </CCol>
+              </CRow>
+            </CFormGroup>
           </CCardBody>
         </CCard>
       </CCol>
